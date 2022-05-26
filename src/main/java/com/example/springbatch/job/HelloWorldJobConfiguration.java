3d67766959557b3a2b5ebbbffae0ad212393b71b
Guangmin Cui <cuiguangmin@gmail.com>
2022-05-26 15:13:11 +0900

    HelloWorldJobConfiguration: JobParameter 추가

--- src/main/java/com/example/springbatch/job/HelloWorldJobConfiguration.java
+++ src/main/java/com/example/springbatch/job/HelloWorldJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,16 +23,18 @@ public class HelloWorldJobConfiguration {
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
-                .start(helloWorldJob_Step1())
+                .start(helloWorldJob_Step1(null))
                 .next(helloWorldJob_Step2())
                 .build();
     }
 
     @Bean
-    public Step helloWorldJob_Step1() {
+    @JobScope
+    public Step helloWorldJob_Step1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("helloWorldJob_Step1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is helloWorldJob_Step1");
+                    log.info(">>>>> JobParameter requestDate={} ", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
